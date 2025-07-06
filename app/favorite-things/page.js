cb025ba62fbfe043f9aea89afe7e21c8fbef5237
Dennis Mullen <dkmullen@gmail.com>
2025-07-06 12:15:54 -0400

    More on music favs

--- app/favorite-things/page.js
+++ app/favorite-things/page.js
@@ -1,7 +1,8 @@
-import MusicCard from "../ui/musicCard"
+import MusicCard from '../ui/musicCard';
 
 const favoriteMusic = [
-  { name: 'johnLegendDancing',
+  {
+    name: 'johnLegendDancing',
     thumbnail: 'https://img.youtube.com/vi/FszBbfxDn4o/hqdefault.jpg',
     alt: 'John Legend at the piano',
     url: 'https://youtu.be/-wVWjl9Kq6U',
@@ -9,9 +10,10 @@ const favoriteMusic = [
     description: `Here is John Legend's wonderful cover of Springsteen's
               <strong>Dancing in the Dark</strong>. And because he is on Howard
               Stern, he sounds amazing. This one brought out the lyrics to me
-              more than the original.`
+              more than the original.`,
   },
-    { name: 'tedeschiMidnight',
+  {
+    name: 'tedeschiMidnight',
     thumbnail: 'https://img.youtube.com/vi/-wVWjl9Kq6U/hqdefault.jpg',
     alt: 'Susan Tedeschi singing',
     url: 'https://youtu.be/20Ov0cDPZy8',
@@ -20,19 +22,131 @@ const favoriteMusic = [
               <strong>Tedeschi Trucks Band,</strong> a great band headed by
               singer Susan Tedeschi and her husband, guitarist Derek Trucks. I
               could listen for hours to any of their performances of
-              <strong>Midnight in Harlem</strong>.`
+              <strong>Midnight in Harlem</strong>.`,
   },
-]
+  {
+    name: 'johnMayerFreeFalling',
+    thumbnail: 'https://img.youtube.com/vi/20Ov0cDPZy8/hqdefault.jpg',
+    alt: 'John Mayer singing',
+    url: 'https://youtu.be/20Ov0cDPZy8',
+    priority: true,
+    description: `I loved Tom Petty's album Full Moon Fever back in the old days,
+              but <strong>John Mayer</strong> (with some help) brings something
+              wonderful to <strong>Free Fallin'</strong>. Really good.`,
+  },
+  {
+    name: 'highWomenChain',
+    thumbnail: 'https://img.youtube.com/vi/jVLNB3d-2cA/hqdefault.jpg',
+    alt: 'The Highwomen performing on Howard Stern',
+    url: 'https://youtu.be/jVLNB3d-2cA',
+    priority: true,
+    description: `I mentioned that every musical artist sounds great on Howard
+              Stern. In my more emotional moments, I like to say that this cover
+              of Fleetwood Mac's <strong>The Chain</strong> by
+              <strong>The Highwomen</strong> is the greatest recording in
+              history. It sounds SOOOO good.`,
+  },
+  {
+    name: 'princeAcoustic',
+    thumbnail: 'https://img.youtube.com/vi/Er1GMEK9-hE/hqdefault.jpg',
+    alt: 'Prince singing and playing guitar',
+    url: '"https://www.youtube.com/watch?v=Er1GMEK9-hE',
+    priority: true,
+    description: `<strong>Prince</strong> was, among many other things, an amazing
+              guitarist. Here he is live with just an acoustic guitar, owning
+              his audience with talent and charm.`,
+  },
+  {
+    name: 'brandosGettysburg',
+    thumbnail: 'https://img.youtube.com/vi/gBrUU-DfihY/hqdefault.jpg',
+    alt: 'The lead singer of the Brandos',
+    url: '"https://www.youtube.com/watch?v=gBrUU-DfihY',
+    priority: true,
+    description: `<strong>Gettysburg</strong> is an 80s rocker from the
+              not-very-well-known <strong>The Brandos</strong>. It's a great
+              song and it always reminds me of a trip I took there with a friend
+              back in college.`,
+  },
+  {
+    name: 'godfathersBirthSchool',
+    thumbnail: 'https://img.youtube.com/vi/QO5dcW0P75M/hqdefault.jpg',
+    alt: 'The lead singer of the Godfathers',
+    url: 'https://www.youtube.com/watch?v=QO5dcW0P75M',
+    priority: true,
+    description: `<strong>Birth, School, Work, Death</strong> is some late New Wave
+              by <strong>The Godfathers</strong> that really delivers a punch.`,
+  },
+  {
+    name: 'billyJoelNYStateofMind',
+    thumbnail: 'https://img.youtube.com/vi/iM4LzEcaTK0/hqdefault.jpg',
+    alt: 'Billy Joel at the piano smoking a cigarette',
+    url: 'https://www.youtube.com/watch?v=iM4LzEcaTK0',
+    priority: true,
+    description: `Here is an astounding live version of
+              <strong>New York State of Mind</strong> from the mid-to-late 70s,
+              when <strong>Billy Joel</strong> was just begining his climb to
+              massive popularity. <strong>Richie Cannata</strong> is the guy
+              with the great sax solo here.`,
+  },
+  {
+    name: 'billySteveWhiteWedding',
+    thumbnail: 'https://img.youtube.com/vi/NOwXqIFikAM/hqdefault.jpg',
+    alt: 'Steve Stevens and Billy Joel performing',
+    url: 'https://www.youtube.com/watch?v=NOwXqIFikAM',
+    priority: true,
+    description: `This acoustic version of<strong> White Wedding</strong> is from
+              2001, and features <strong>Billy Idol</strong> in fine voice, but
+              I'm especially impressed with the always great
+              <strong>Steve Stevens</strong> on guitar.`,
+  },
+  {
+    name: 'acdcLongWay',
+    thumbnail: 'https://img.youtube.com/vi/g-qkY2yj4_A/hqdefault.jpg',
+    alt: 'Bon Scott and ACDC performing on a city street',
+    url: 'https://youtu.be/g-qkY2yj4_A?si=W9e7NDwJHYn3oU-e',
+    priority: true,
+    description: `I somehow missed this, the best of all
+              <strong>AC/DC</strong> tracks till it was featured over the
+              credits of Jack Black's <em>School of Rock</em>, but
+              <strong>It's a Long Way to the Top</strong> is now an all-time
+              favorite.`,
+  },
+  {
+    name: 'heartStairway',
+    thumbnail: 'https://img.youtube.com/vi/LFxOaDeJmXk/hqdefault.jpg',
+    alt: 'Ann and Nancy Wilson on a large stage, under spotlights',
+    url: 'https://www.youtube.com/watch?v=LFxOaDeJmXk',
+    priority: true,
+    description: `From the Kennedy Center Honors in 2012, here's the second-best
+              recording of <strong> Stairway to Heaven</strong>. This
+              performance is in honor of <strong>Led Zepplin</strong>, and
+              features <strong>Ann and Nancy Wilson, Jason Bonham</strong> taking his
+              father's place on drums, and a great choir.`,
+  },
+  {
+    name: 'tommyShawFoolingYourself',
+    thumbnail: 'https://img.youtube.com/vi/xpYHgbjOIq0/hqdefault.jpg',
+    alt: 'Tommy Shaw on stage with a youth orchestra',
+    url: 'https://www.youtube.com/watch?v=xpYHgbjOIq0',
+    priority: true,
+    description: `I love this. It's <strong>Fooling Yourself</strong> by
+              <strong>Tommy Shaw</strong> backed up by the awesome
+              <strong>Contemporary Youth Orchestra</strong>. Lots of joy on the
+              faces of the performers and the audience. Watch the blonde girl in
+              the crowd get caught up while Papaw beside her is transported back
+              to when he saw <strong>Styx</strong> live in '78.`,
+  },
+];
 
 export default function Page() {
   return (
-    <div className="page">
+    <div className='page'>
       <p>Favorite Things</p>;
-            {favoriteMusic.map((item) => (
-              <MusicCard  key={item.name} item={item} />
+      {favoriteMusic.map((item) => (
+        <MusicCard key={item.name} item={item} />
       ))}
     </div>
-  )
+  );
 }
 
-// loading="eager" priority={true} 
+// loading="eager" priority={true}
--- app/ui/musicCard.js
+++ app/ui/musicCard.js
@@ -1,28 +1,41 @@
-import Image from "next/image"
-import DOMPurify from 'isomorphic-dompurify'
+import Image from 'next/image';
+import Link from 'next/link';
+import DOMPurify from 'isomorphic-dompurify';
+import styles from './ui.module.css';
 
 export default function MusicCard({ item }) {
-  const safeHTML = DOMPurify.sanitize(item.description)
+  const safeHTML = DOMPurify.sanitize(item.description);
 
-
-    return (
+  return (
     <>
-    <div>
-        <div style={{ position: 'relative', width: '100%', height: 'auto',  aspectRatio: '4 / 3', maxWidth: '480px' }}>
-            <Image 
-                className="video-thumbnail" 
-                src={item.thumbnail}
-                alt={item.alt}
-                url={item.url}
-                priority={item.priority || false}
-                fill
-                style={{ objectFit: 'contain' }} // or 'cover' or 'fill' depending on your needs
-        sizes="100%"
+      <div className={styles['card-wrapper']}>
+        <Link href={item.url}>
+          <div
+            style={{
+              position: 'relative',
+              width: '100%',
+              height: 'auto',
+              aspectRatio: '4 / 3',
+              maxWidth: '480px',
+            }}
+          >
+            <Image
+              className={styles['video-thumbnail']}
+              src={item.thumbnail}
+              alt={item.alt}
+              url={item.url}
+              priority={item.priority || false}
+              fill
+              style={{ objectFit: 'contain' }} // or 'cover' or 'fill'
+              sizes='100%'
             />
-        </div>
-      <p dangerouslySetInnerHTML={{ __html: safeHTML }} />
-    </div>
+          </div>
+          <p
+            className={styles['video-description']}
+            dangerouslySetInnerHTML={{ __html: safeHTML }}
+          />
+        </Link>
+      </div>
     </>
-
-    )
+  );
 }
