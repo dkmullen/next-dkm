ef55a22e470ec7d196c5ab1f3b07fe49cfb5c847
Dennis Mullen <dkmullen@gmail.com>
2025-07-14 10:50:28 -0400

    More on music favs and sub nav

--- app/favorite-things/page.js
+++ app/favorite-things/page.js
@@ -1,4 +1,7 @@
 import MusicCard from '../ui/musicCard';
+import styles from './page.module.css';
+import clsx from 'clsx';
+import Link from 'next/link';
 
 const favoriteMusic = [
   {
@@ -140,11 +143,25 @@ const favoriteMusic = [
 
 export default function Page() {
   return (
-    <div className='page'>
-      <p>Favorite Things</p>;
-      {favoriteMusic.map((item) => (
-        <MusicCard key={item.name} item={item} />
-      ))}
+    <div className={styles['music-page']}>
+      <div className={clsx('center', styles['title-bar'])}>
+        <h2>Favorite Things - Music</h2>
+        <p>
+          Some of my favorite music,{' '}
+          <Link href='favorite-things/books'>books</Link> and{' '}
+          <Link href='favorite-things/other-things'>other things</Link> on the
+          web.
+        </p>
+      </div>
+      <div id={styles['music-grid']}>
+        {favoriteMusic.map((item) => (
+          <MusicCard
+            key={item.name}
+            item={item}
+            className={styles['music-card']}
+          />
+        ))}
+      </div>
     </div>
   );
 }
